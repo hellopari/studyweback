c6612378c2d30137e0f1e514b7b59469826b6006
zhujiaojiao <zhujiaojiao@dm-ai.com>
2021-09-01 18:34:42 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # studyweback
 study weback
+
+
+```运行npx webpack或者npm run build
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [new HtmlWebpackPlugin()]
+};
